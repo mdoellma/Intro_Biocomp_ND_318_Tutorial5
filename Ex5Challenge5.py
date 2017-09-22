12dc2cd03d9c046b6fbd8e2a64150e19409d70cc
Grant Keller <gkeller1@nd.edu>
2017-09-22 14:49:14 -0400

    Added in some documentation to explain the function setup for Exercise 5.

--- Ex5Challenge5.py
+++ Ex5Challenge5.py
@@ -1,22 +1,33 @@
 #!/bin/python
 
+import pandas, numpy
 
+def Q1solution():
+	return
 
+def Q2solution(wages):
+    """
+    Assume that pandas and numpy have already been imported and that you have
+    access to wages.csv as the pandas dataframe object wages. Leave the return,
+    write your code (indented) between the "def Q2solution" and the "return"
+    """
+    
+    
+    return
 
+def Q3solution():
 
 
 if __name__ == '__main__':
-	"""
-	Docstring tests to see if solutions are working correctly.
-
-	Don't change anything down here.
-	
-	
-
-
-	"""
-	Q1solution()
-	
-	Q2solution()
-	
-	Q3solution()
+    """
+    Docstring tests to see if solutions are working correctly.
+    Don't change anything down here.
+    >>> d = {''}
+    >>> Q1test = 
+    """
+    wages = pandas.read_csv("wages.csv")
+    Q1solution()
+    
+    Q2solution()
+    
+    Q3solution()
