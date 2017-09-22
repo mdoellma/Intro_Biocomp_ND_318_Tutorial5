8eec087aaaad83d18228555425fc49e1db3a1a54
Grant Keller <gkeller1@nd.edu>
2017-09-22 11:22:25 -0400

    Initial commit for collaboration.

---
+++ Ex5Challenge5.py
@@ -0,0 +1,22 @@
+#!/bin/python
+
+
+
+
+
+
+if __name__ == '__main__':
+	"""
+	Docstring tests to see if solutions are working correctly.
+
+	Don't change anything down here.
+	
+	
+
+
+	"""
+	Q1solution()
+	
+	Q2solution()
+	
+	Q3solution()
