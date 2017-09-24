91852e058ce432ea20680f6b7a4e9bad9fdc7f78
janicelove <jlove5@nd.edu>
2017-09-23 23:20:21 -0400

    part of Q1: need to remove line numbers from output file.

--- challenge.py
+++ challenge.py
@@ -3,14 +3,28 @@
 #Date: 2017.09.23
 
 #Check working directory!!
-import numpy 
+
+f = open('myoutput.txt' ,'w+')
+print f 
+
+
+import numpy
 import pandas
 wages = pandas.read_csv("wages.csv")
-wages.drop_duplicates()
-wages.loc[:, 'gender':'yearsExperience'] 
-Q1 = wages.loc[:, 'gender': 'yearsExperience']
-gender_sorted = Q1.sort_values('gender')#sort by gender first
-print gender_sorted.head(n=30)
-y = gender_and_years.drop_duplicates()
-print y.head(n=20)
 
+y = wages.drop_duplicates('yearsExperience')
+
+Q1 = y.loc[:, 'gender': 'yearsExperience']
+
+gender_sorted = Q1.sort_values('gender') #sort by gender first
+print gender_sorted.head(n=30),
+
+z = gender_sorted.sort_index(axis = 1)
+print z,
+sorted_data = z.sort_values('yearsExperience')
+print sorted_data 
+
+sorted_data.to_csv(path_or_buf=f, sep=',')
+
+
+f.close()
