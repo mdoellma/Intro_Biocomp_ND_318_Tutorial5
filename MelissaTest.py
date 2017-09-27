f117911eb73174602cd70fc14273ce9f6093f921
Melissa Stephens <mpullins@nd.edu>
2017-09-27 19:10:45 -0400

    returns highest and lowest earner in dataset

--- MelissaTest.py
+++ MelissaTest.py
@@ -7,6 +7,8 @@ column=data.iloc[0:,:4]
 print(column)
 w = data.sort_values('wage', ascending = False)
 w=w.iloc[0:1]
-print(w)
-
+print("The highest Earner is",w)
+w = data.sort_values('wage', ascending = False)
+w=w.iloc[-1]
+print("The Lowest Earner is",w)
 
--- challenge.py
+++ challenge.py
@@ -32,13 +32,17 @@ f.close()
 
 #Q2 code is below 
 import pandas
-wages = pandas.read_csv("wages.csv", delimiter=",")
+wages=pandas.read_csv("wages.csv", delimiter=",")
 
-column=data.iloc[0:,:4]
+#Returns Higest and Lowest Earner in Dataset
+column=wages.iloc[0:,:4]
 print(column)
-w = data.sort_values('wage', ascending = False)
-w=w.iloc[0:1]
-print(w)
+High=wages.sort_values('wage', ascending = False)
+High=High.iloc[0:1]
+print("The highest earner is:",High)
+Low=wages.sort_values('wage', ascending = False)
+Low=Low.iloc[-1]
+print("The lowest earner is:",Low)
 
 
 w = wages.sort_values('wage', ascending = False)
