893e6c8172cf971f0e61209149232bbb88b19dec
Grant Keller <gkeller1@nd.edu>
2017-09-27 15:02:44 -0400

    Rewrote q1solution to utilize pandas dataframe methods and better sorting. Refactored functions and constant to fit PEP 8 specifications.

--- Ex5Challenge5.py
+++ Ex5Challenge5.py
@@ -8,25 +8,19 @@ from __future__ import print_function
 import pandas
 import numpy
 
-def Q1solution(wages):
+def q1solution(wages):
     """
     Writes out a file containing all the unique gender-yearsExperience pairs
     in the data set given (wages.csv). These are written to file
     "gender-yearsExperience.txt" saved in the current working directory.
     """
-    ordered = wages.sort_values("yearsExperience")
-    ordered = ordered.reset_index(drop=True)
-    outText = []
-    for i in range(ordered.shape[0]):
-        pair = "{} {}".format(ordered.gender[i], ordered.yearsExperience[i])
-        if pair not in outText:
-            outText.append(pair)
-    outFile = open('gender-yearsExperience.txt', 'w')
-    print('\n'.join(outText), file=outFile)
-    outFile.close()
+    ordered = wages.drop(["yearsSchool", "wage"], axis=1)
+    ordered.sort_values(["gender", "yearsExperience"], inplace=True)
+    ordered.drop_duplicates(inplace=True)
+    ordered.to_csv('gender-yearsExperience.txt', sep=' ', index=False)
     return
 
-def Q2solution(wages):
+def q2solution(wages):
     """
     #############################
     Assume that pandas and numpy have already been imported and that you have
@@ -34,36 +28,35 @@ def Q2solution(wages):
     return, write your code (indented) between the "def Q2solution" and the
     "return"
     #############################
-    >>> testG = ["male","female","male","male","female","male","male","male","female","male","male"]
-    >>> testYE = [6,5,4,1,1,10,8,2,5,3,4]
-    >>> testYS = [12,12,16,14,13,16,18,6,16,12,21]
-    >>> testW =[2.09405811,12.861342479,6.1056621467999994,3.9808917197,5.6311238472,1.3456535391,3.8947576562,1.3551971812,4.4437861058,3.9808917197,0.086111532]
+    >>> testG = ["male","female","male","male","female","male","male","male","female","male"]
+    >>> testYE = [6,5,4,1,1,10,8,2,5,4]
+    >>> testYS = [12,12,16,14,13,16,18,6,16,21]
+    >>> testW =[2.094,12.861,6.105,3.980,5.631,1.345,3.894,1.355,4.443,0.086]
     >>> d = {'gender' : testG,'yearsExperience' : testYE,'yearsSchool' : testYS,'wage' : testW}
     >>> df = pandas.DataFrame(data=d)
     >>> Q2solution(df)
-    The highest earner makes 12.861342479, is female and has 5 years of experience.
-    The lowest earner makes 0.086111532, is male and has 4 years of experience.
+    The highest earner makes 12.861, is female and has 5 years of experience.
+    The lowest earner makes 0.086, is male and has 4 years of experience.
     There are 3 females in the top ten earners.
     """
-    
     return
 
-def Q3solution(wages):
+def q3solution(wages):
     """
     Prints to stdout the difference in minimum wage for yearsSchool 12 (HS)
     and 16 (college) in this dataset (wages.csv).
-    
+    ################
     >>> testG = ["male","female","male","male","female","male","male","male","female","male","male"]
     >>> testYE = [6,5,4,1,1,10,8,2,5,3,4]
     >>> testYS = [12,16,12,14,13,12,18,6,16,16,21]
-    >>> testW =[2.09405811,12.861342479,6.1056621467999994,3.9808917197,5.6311238472,1.3456535391,3.8947576562,1.3551971812,4.4437861058,3.9808917197,0.086111532]
+    >>> testW =[2.094,12.861,6.105,3.980,5.631,1.345,3.894,1.355,4.443,3.98,0.086]
     >>> d = {'gender' : testG,'yearsExperience' : testYE,'yearsSchool' : testYS,'wage' : testW}
     >>> df = pandas.DataFrame(data=d)
     >>> Q3solution(df)
-    Those with a 4 year degree (16 yearsSchool) earned 2.6352381806 more than those with just a high school degree (12 yearsSchool).
+    Those with a 4 year degree (16 yearsSchool) earned 2.635 more than those with just a high school degree (12 yearsSchool).
     """
-    minWage = lambda wages,num: min(wages.wage[wages.yearsSchool == num])
-    print("Those with a 4 year degree (16 yearsSchool) earned {0} more than those with just a high school degree (12 yearsSchool).".format(minWage(wages, 16)-minWage(wages, 12)))
+    min_wage = lambda wages, num: min(wages.wage[wages.yearsSchool == num])
+    print("Those with a 4 year degree (16 yearsSchool) earned {0} more than those with just a high school degree (12 yearsSchool).".format(min_wage(wages, 16)-min_wage(wages, 12)))
     return
 
 ################################
@@ -72,8 +65,8 @@ if __name__ == '__main__':
     import doctest
     doctest.testmod()
     print("Done.")
-    wages = pandas.read_csv("wages.csv")    
-    Q1solution(wages)
-    Q2solution(wages)
-    Q3solution(wages)
+    WAGES = pandas.read_csv("wages.csv")
+    q1solution(WAGES)
+    q2solution(WAGES)
+    q3solution(WAGES)
     
