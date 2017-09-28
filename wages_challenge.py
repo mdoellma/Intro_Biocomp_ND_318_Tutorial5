bb0f32d535e2219ec0089172e875fc6a4786f4af
Aaron Long <along10@nd.edu>
2017-09-28 15:49:13 -0400

    cleaned up code

--- wages_challenge.py
+++ wages_challenge.py
@@ -19,36 +19,20 @@ print(ge)
 #--Start Challenge 2
 #return gender, yearsExp, and wage for highest earner
 highest_earner = wages.sort_values(['wage']).iloc[len(wages) - 1]
+print("Highest Earner:")
 print(highest_earner)
 
 #return gender, yearsExp, and wage for highest earner
 lowest_earner = wages.sort_values(['wage']).iloc[0]
+print("Lowest Earner:")
 print(lowest_earner)
 
-#return number of females in top ten earnesr
+#return number of females in top ten earners
 top_ten = wages.sort_values(['wage'],ascending=False).iloc[0:10]
 print(len(top_ten[top_ten.gender=="female"]))
 #-- End Challenge 2
 
 #--Start Challenge 3
-#Returns the yearsExp and wage columns
-minmax=wages.iloc[0:,2:]
-
-#Sorts the set by the yearsExp column
-minmax=minmax.sort_values(['yearsSchool'])
-minmax
-
-#Finds and sets a variable for the lowest 16yr earner and lowest 12yr earner
-min=minmax.head(n=1)
-min=min.iloc[0,1]
-max=minmax.tail(n=1)
-max=max.iloc[0,1]
-
-#Does the aritmetic and prints the value
-D=max-min
-print('Increase in minimum wage after graduating college is' , D)
-
-#---------------- 
 # get separate dataframes for yearsSchool and sort by wage, lowest first
 years_12 = wages[wages.yearsSchool == 12].sort_values(['wage'])
 years_16 = wages[wages.yearsSchool == 16].sort_values(['wage'])
@@ -59,6 +43,4 @@ minwage_16 = years_16.iloc[0].wage
 
 #output
 print('Increase in minimum wage after graduating college is', minwage_16 - minwage_12)
-
-
 #--End of Challenge 3--
